0e0c50168e4152684ecedcf95a5875c12e0268eb
DhimMhorisom <dhim.ads1@gmail.com>
2026-04-08 19:08:09 -0300

    feat: secure routes with JWT authentication filter

---
+++ orderflow/src/main/java/com/dhimmhorisom/orderflow/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.dhimmhorisom.orderflow.security;
+
+import com.dhimmhorisom.orderflow.entity.User;
+import com.dhimmhorisom.orderflow.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
